ef0d46230b38dfe879aa10fa96a36372eb536863
catchthefloaty <catchthefloaty@yahoo.com>
2015-04-18 06:52:35 -0700

    broken firing mechanism

--- ludumDare32/Assets/Scripts/Player.cs
+++ ludumDare32/Assets/Scripts/Player.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     SpriteRenderer texture;
     public Sprite[] IdleAnimations;
     public float[] IdleTimings;
@@ -13,20 +14,30 @@ public class Player : MonoBehaviour {
     int GameplayState = 1;
     Rigidbody2D rb;
     public float moveSpeed = 3;
-    
+
     int AnimState;
     int curFrame;
     int h;
     int v;
-	// Use this for initialization
-	void Start () {
+
+    Vector3 mouseclick;
+    public GameObject bullet1;
+    float fireTime;
+    public float fireRate;
+    // Use this for initialization
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         texture = GetComponent<SpriteRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-    //Gameplay stuff
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        //Gameplay stuff
+        fireTime += Time.deltaTime;
         if (GameplayState == 0)
         {
             if (Mathf.Abs(Input.GetAxis("Horizontal")) + Mathf.Abs(Input.GetAxis("Vertical")) > 0)
@@ -37,12 +48,18 @@ public class Player : MonoBehaviour {
                 texture.sprite = WalkAnimations[curFrame];
                 AnimTime = 0;
             }
+            if (Input.GetMouseButtonDown(0) == true)
+            {
+                GameplayState = 2;
+                mouseclick = Input.mousePosition;
+            }
         }
         else if (GameplayState == 1)
         {
             h = 0;
             v = 0;
-            if (Input.GetAxisRaw("Horizontal") > 0) {
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
                 h = 1;
             }
             else if (Input.GetAxisRaw("Horizontal") < 0)
@@ -57,7 +74,7 @@ public class Player : MonoBehaviour {
             {
                 v = -1;
             }
-            transform.Translate(h * Time.deltaTime * moveSpeed, v * Time.deltaTime * moveSpeed,0);
+            transform.Translate(h * Time.deltaTime * moveSpeed, v * Time.deltaTime * moveSpeed, 0);
             //rb.AddForce(new Vector2(Input.GetAxis("Horizontal")*Time.deltaTime * moveSpeed,Input.GetAxis("Vertical")* Time.deltaTime*moveSpeed));
 
 
@@ -70,17 +87,23 @@ public class Player : MonoBehaviour {
                 AnimTime = 0;
             }
         }
+        else if (GameplayState == 2)
+        {
+            FireBullet1(mouseclick);
+            GameplayState = 0;
+        }
 
 
-	//animation stuffwd
+        //animation stuffwd
         if (AnimState == 0)
         {
             AnimTime += Time.deltaTime;
-            if (AnimTime > IdleTimings[curFrame] + Random.Range(0f,IdleTimings[curFrame]) )
+            if (AnimTime > IdleTimings[curFrame] + Random.Range(0f, IdleTimings[curFrame]))
             {
                 AnimTime = 0;
                 curFrame++;
-                if (curFrame >= IdleAnimations.GetLength(0)){
+                if (curFrame >= IdleAnimations.GetLength(0))
+                {
                     curFrame = 0;
                 }
                 texture.sprite = IdleAnimations[curFrame];
@@ -100,5 +123,14 @@ public class Player : MonoBehaviour {
                 texture.sprite = WalkAnimations[curFrame];
             }
         }
-	}
+    }
+    void FireBullet1(Vector3 target)
+    {
+        Vector3 degVector = (target - transform.position).normalized;
+        float deg = Vector3.Angle(transform.position,target);
+        
+        Debug.Log(deg);
+    }
 }
+
+
