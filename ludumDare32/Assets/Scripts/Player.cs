f0cd8c512118bd1807a5c69209f39922ff82b4f8
catchthefloaty <catchthefloaty@yahoo.com>
2015-04-18 07:53:05 -0700

    targeting fixed

--- ludumDare32/Assets/Scripts/Player.cs
+++ ludumDare32/Assets/Scripts/Player.cs
@@ -144,7 +144,7 @@ public class Player : MonoBehaviour
 
         float rot_z = Mathf.Atan2(tempuntilbulletismade.y, tempuntilbulletismade.x) * Mathf.Rad2Deg;
         //temp.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
-        Debug.Log(rot_z+90);
+        Debug.Log(Mathf.Abs(Quaternion.Euler(0f, 0f, rot_z - 90).eulerAngles.z-360));
         
     }
 }
