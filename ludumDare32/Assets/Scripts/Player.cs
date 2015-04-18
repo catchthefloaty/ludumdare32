144bf6376015f25fa0ecd85bf5dfe910e7ee83f0
catchthefloaty <catchthefloaty@yahoo.com>
2015-04-17 23:38:06 -0700

    movement fix

--- ludumDare32/Assets/Scripts/Player.cs
+++ ludumDare32/Assets/Scripts/Player.cs
@@ -16,6 +16,8 @@ public class Player : MonoBehaviour {
     
     int AnimState;
     int curFrame;
+    int h;
+    int v;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
@@ -31,8 +33,26 @@ public class Player : MonoBehaviour {
         }
         else if (GameplayState == 1)
         {
-            transform.Translate(Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed, Input.GetAxisRaw("Vertical") * Time.deltaTime * moveSpeed,0);
+            h = 0;
+            v = 0;
+            if (Input.GetAxisRaw("Horizontal") > 0) {
+                h = 1;
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                h = -1;
+            }
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                v = 1;
+            }
+            else if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                v = -1;
+            }
+            transform.Translate(h * Time.deltaTime * moveSpeed, v * Time.deltaTime * moveSpeed,0);
             //rb.AddForce(new Vector2(Input.GetAxis("Horizontal")*Time.deltaTime * moveSpeed,Input.GetAxis("Vertical")* Time.deltaTime*moveSpeed));
+            
         }
 
 
