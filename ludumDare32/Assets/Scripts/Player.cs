5a4ad56ba783f89587ff320386de9a63e04f3158
catchthefloaty <catchthefloaty@yahoo.com>
2015-04-17 23:20:36 -0700

    movement

--- ludumDare32/Assets/Scripts/Player.cs
+++ ludumDare32/Assets/Scripts/Player.cs
@@ -7,22 +7,36 @@ public class Player : MonoBehaviour {
     public Sprite[] IdleAnimations;
     public float[] IdleTimings;
     public float[] IdleTimingOffsets;
+    public Sprite[] WalkAnimations;
+    public float[] WalkTimings;
     float AnimTime;
-    //public float AnimRate = 3;
+    int GameplayState = 1;
+    Rigidbody2D rb;
+    public float moveSpeed = 3;
+    
     int AnimState;
     int curFrame;
 	// Use this for initialization
 	void Start () {
+        rb = GetComponent<Rigidbody2D>();
         texture = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
     //Gameplay stuff
+        if (GameplayState == 0)
+        {
+            
+        }
+        else if (GameplayState == 1)
+        {
+            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed,0);
+            //rb.AddForce(new Vector2(Input.GetAxis("Horizontal")*Time.deltaTime * moveSpeed,Input.GetAxis("Vertical")* Time.deltaTime*moveSpeed));
+        }
 
 
-
-	//animation stuff
+	//animation stuffwd
         if (AnimState == 0)
         {
             AnimTime += Time.deltaTime;
@@ -34,6 +48,20 @@ public class Player : MonoBehaviour {
                     curFrame = 0;
                 }
                 texture.sprite = IdleAnimations[curFrame];
+            }
+        }
+        else if (AnimState == 1)
+        {
+            AnimTime += Time.deltaTime;
+            if (AnimTime > WalkTimings[curFrame])
+            {
+                AnimTime = 0;
+                curFrame++;
+                if (curFrame >= WalkAnimations.GetLength(0))
+                {
+                    curFrame = 0;
+                }
+                texture.sprite = WalkAnimations[curFrame];
             }
         }
 	}
