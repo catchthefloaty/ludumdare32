a7d8f14c4dd2e8b0ce784cedf44342805f49091f
catchthefloaty <catchthefloaty@yahoo.com>
2015-04-18 20:44:34 -0700

    camera controls.

---
+++ ludumDare32/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    GameObject p;
+    public float distance;
+    public int followStatex =1;
+    public int followStatey = 1;
+
+	// Use this for initialization
+	void Start () {
+        p = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ( followStatex == 1 && followStatey == 1){
+        transform.position = new Vector3(p.transform.position.x, p.transform.position.y, -50);
+	}
+        if (followStatex == 1)
+        {
+            transform.position = new Vector3(p.transform.position.x, transform.position.y, -50);
+        }
+        if (followStatey == 1)
+        {
+            transform.position = new Vector3(transform.position.x, p.transform.position.y, -50);
+        }
+    }
+
+}
--- ludumDare32/Assets/Scripts/Player.cs
+++ ludumDare32/Assets/Scripts/Player.cs
@@ -152,6 +152,8 @@ public class Player : MonoBehaviour
         
         
     }
+
+    
 }
 
 
