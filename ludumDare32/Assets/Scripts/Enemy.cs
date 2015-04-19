0647a6edb56df5b6a3dfa9d761d1de41180f66a2
catchthefloaty <catchthefloaty@yahoo.com>
2015-04-18 18:09:56 -0700

    enemy fires from mouth

--- ludumDare32/Assets/Scripts/Enemy.cs
+++ ludumDare32/Assets/Scripts/Enemy.cs
@@ -233,16 +233,16 @@ public class Enemy : MonoBehaviour
 
     void Attack()
     {
-        GameObject Bullet = (GameObject)GameObject.Instantiate(bullet1, transform.position, Quaternion.identity);
+        GameObject Bullet = (GameObject)GameObject.Instantiate(bullet1, transform.GetChild(0).transform.position, Quaternion.identity);
         Bullet.GetComponent<EnemyBullet>().direction = new Vector3(0, -1f,0);
         Bullet.GetComponent<SpriteRenderer>().sprite = bullets[Random.Range(0,bullets.GetLength(0))];
-        Bullet = (GameObject)GameObject.Instantiate(bullet1, transform.position, Quaternion.identity);
+        Bullet = (GameObject)GameObject.Instantiate(bullet1, transform.GetChild(0).transform.position, Quaternion.identity);
         Bullet.GetComponent<EnemyBullet>().direction = new Vector3(-1f, 0, 0);
         Bullet.GetComponent<SpriteRenderer>().sprite = bullets[Random.Range(0, bullets.GetLength(0))];
-        Bullet = (GameObject)GameObject.Instantiate(bullet1, transform.position, Quaternion.identity);
+        Bullet = (GameObject)GameObject.Instantiate(bullet1, transform.GetChild(0).transform.position, Quaternion.identity);
         Bullet.GetComponent<EnemyBullet>().direction = new Vector3(0, 1f, 0);
         Bullet.GetComponent<SpriteRenderer>().sprite = bullets[Random.Range(0, bullets.GetLength(0))];
-        Bullet = (GameObject)GameObject.Instantiate(bullet1, transform.position, Quaternion.identity);
+        Bullet = (GameObject)GameObject.Instantiate(bullet1, transform.GetChild(0).transform.position, Quaternion.identity);
         Bullet.GetComponent<EnemyBullet>().direction = new Vector3(1f, 0, 0);
         Bullet.GetComponent<SpriteRenderer>().sprite = bullets[Random.Range(0, bullets.GetLength(0))];
     }
