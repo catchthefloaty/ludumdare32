a47346cbe8bc49251eea0c9435a4db27166befd6
catchthefloaty <catchthefloaty@yahoo.com>
2015-04-19 17:35:52 -0700

    bullet fixes prefabs for each enemy

---
+++ ludumDare32/Assets/Scripts/levelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelManager : MonoBehaviour {
+    //GameObject[] enemies;
+    public string nextlevel;
+    public int enemycount;
+	// Use this for initialization
+	void Start () {
+        enemycount = GameObject.FindGameObjectsWithTag("Enemy").GetLength(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (enemycount == 0)
+        {
+            Application.LoadLevel(nextlevel);
+        }
+	
+	}
+}
