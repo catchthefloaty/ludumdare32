e4fa8eee1909e36ba090bf6c5ffc6c0fb5bd399a
catchthefloaty <catchthefloaty@yahoo.com>
2015-04-19 18:00:14 -0700

    camera fix

--- ludumDare32/Assets/Scripts/CameraFollow.cs
+++ ludumDare32/Assets/Scripts/CameraFollow.cs
@@ -11,7 +11,7 @@ public class CameraFollow : MonoBehaviour {
     public float f = .015f;
 	// Use this for initialization
 	void Start () {
-        p = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).gameObject;
+        p = GameObject.FindGameObjectWithTag("Player").transform.GetChild(1).gameObject;
 	}
 	
 	// Update is called once per frame
